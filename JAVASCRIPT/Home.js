adbce266c22d9ce1c2d4d55493db41c1c250efd5
Nakui <bayulujab@gmail.com>
2019-09-21 13:07:17 -0600

    Create Home.js

---
+++ JAVASCRIPT/Home.js
@@ -0,0 +1,52 @@
+//Llama el modal
+/*var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+
+// Obtiene el boton que abre el modal
+var btn2 = document.getElementById("btn2");
+var btn1 = document.getElementById("btn1");
+var btn3 = document.getElementById("btn3");
+var btn4 = document.getElementById("btn4");
+var btn5 = document.getElementById("btn5");
+
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close2")[0];
+
+//Se abre el modal
+btn2.onclick = function() {
+  modal.style.display = "block";
+}
+
+btn1.onclick = function() {
+  modal2.style.display = "block";
+}
+
+btn3.onclick = function() {
+  modal2.style.display = "block";
+}
+
+btn4.onclick = function() {
+  modal2.style.display = "block";
+}
+
+btn5.onclick = function() {
+  modal2.style.display = "block";
+}
+
+//Cuando presione el boton se cierra el modal
+span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+span2.onclick = function() {
+    modal2.style.display = "none";
+  }
+
+//Cuando presiono fuera del modal se cierra
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    } else if (event.target == modal2) {
+      modal2.style.display = "none";
+    }
+  }*/
