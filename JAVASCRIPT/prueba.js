0a00abe6bafa2a861c0302819acb7131cda560f5
Nakui <bayulujab@gmail.com>
2019-09-21 13:07:47 -0600

    Create prueba.js

---
+++ JAVASCRIPT/prueba.js
@@ -0,0 +1,120 @@
+/*
+<!---- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <form class="modal-content">
+  	<span class="close">&times;</span> <!--&times; funciona como x para un boton cerrar-->
+    <label class="d1">Hi! Welcome back!</label>
+    <label class="d2">You are about to get more insights on your Loans</label>
+    <button class="b1"><label class="b11">G</label><label class="b12">&nbsp; | &nbsp;</label>LOGIN USING GOOGLE</button>
+	<div class="contenedor">
+    <label class="d3">OR</label>
+    <label class="d4">EMAIL:</label>
+    <input class="t1"></input>
+    <label class="d5">PASSWORD:</label>
+    <input class="t2" type="PASSWORD"></input>
+    <button class="b2">LOGIN TO YOUR ACCOUNT</button>
+	</div>
+  </form>
+  </div>
+
+  <!---- The Modal -->
+<div id="myModal2" class="modal2">
+
+  <!-- Modal content -->
+  <form class="modal-content2">
+  	<span class="close2">&times;</span> <!--&times; funciona como x para un boton cerrar-->
+    <label class="d1b">Almost there!</label>
+    <label class="d2b">You are about to get more insights on your Loans</label>
+    <button class="b1b"><label class="b11b">G</label><label class="b12b">&nbsp; | &nbsp;</label>LOGIN USING GOOGLE</button>
+	<div class="contenedor2">
+    <label class="d3b">OR</label>
+    <label class="d4b">EMAIL:</label>
+    <input class="t1b"></input>
+    <label class="d5b">PASSWORD:</label>
+    <input class="t2b" type="PASSWORD"></input>
+    <button class="b2b">CREATE ACCOUNT</button>
+	</div>
+  </form>
+  </div>
+
+
+<div id="fondo2">
+	<label id="parrafo3">Save your Loans, get insights</label>
+	<br>
+	<label id="parrafo4">Register, record and track all your loans</label>
+</div>
+
+	<div id="tablePrestamos">				
+		<img src="../assets/prestamos.jpg"/>
+	</div>
+
+
+<div id="fondo3">
+	
+	<div id="part3">
+  		<p id="parrafoBajo">Alerts and notifications</p>
+  		<p id="parrafoBajo2">Is your free overdue? Is it a good idea to pay to debt?
+		  Should I transfer my loan to another bank? is it a<br>
+		  good time for that?</p>
+  		<button id="btn4">COMIENZA AHORA</button>
+	</div>
+
+	<form id="juntos">
+	<table id="table2">
+		<tr>
+			<th>
+				<img src="../assets/8.jpg"/>
+			</th>
+				<th id="parrafo5">Recuerda realizar el pago de tu <br>préstamo "Préstamo de Casa"</th>
+		</tr>
+		<tr>
+                <th>
+                    <p></p>
+                    <p></p>
+                </th>
+            </tr>
+		<tr>
+			<th>
+				<img src="../assets/9.jpg"/>
+				<th id="parrafo6">En dos meses terminarás tu<br>prestamo<b>"Préstamo de Casa"...</b></th>
+			</th>
+		</tr>
+		<tr>
+                <th>
+                    <p></p>
+                    <p></p>
+                </th>
+            </tr>
+		<tr>	
+			<th>
+				<img src="../assets/10.jpg"/>
+				<th id="parrafo7">Prodías obtener una mejor opción<br>bancaria de tu préstamo...</th>
+			</th>
+		</tr>
+		</table>
+	</form>
+
+</div>
+
+</body>
+
+<footer id="pie">
+	<br>
+  <label id="pieLabel1">Export and share your analytics</label>
+  <label id="pieLabel2">Send your loan info via email or print a document.</label>
+ 
+<div id="tabla">
+	<table>
+		<tr>
+			<td><img id="im1" src="../assets/1.jpg"/></td>
+			<td><img id="im2" src="../assets/2.jpg"/></td>
+			<td><img id="im3" src="../assets/3.jpg"/></td>
+		</tr>
+	</table>
+</div>
+<br>
+<footer id="pie2"><label class="creditos"> ⓒ www.myloans.com.gt | Todos los derechos reservados | Guatemala - Guatemala 2016</label></footer>
+</footer>
+*/
